07de16a6df6c9c1adbeb4c7e2d2c43aedffcb781
serj <13497925+gamedevserj@users.noreply.github.com>
2025-01-30 15:03:53 +0400

    Added config data

---
+++ Assets/UISystem/Scripts/Common/ConfigData.cs
@@ -0,0 +1,30 @@
+﻿using UISystem.Common.Enums;
+using UnityEngine;
+
+namespace UISystem.Constants
+{
+    public static class ConfigData
+    {
+
+        public const string ConfigLocation = "Config.cfg";
+
+        public const string AudioSectionName = "AudioSettings";
+        public const string MusicVolumeKey = "MusicVolume";
+        public const float DefaultMusicVolume = 0.5f;
+        public const string SfxVolumeKey = "SfxVolume";
+        public const float DefaultSfxVolume = 0.5f;
+
+        public const string VideoSectionName = "VideoSettings";
+        public const string ResolutionKey = "Resolution";
+        //public static Vector2I DefaultResolution => ScreenGetSize();
+        public const string WindowModeKey = "WindowMode";
+        //public const WindowMode DefaultWindowMode = WindowMode.ExclusiveFullscreen;
+
+        public const string KeysSectionName = "Keys";
+
+        public const string InterfaceSectionName = "Interface";
+        public const string ControllerIconsKey = "ControllerIcons";
+        public const ControllerIconsType DefaultControllerIconsType = ControllerIconsType.Xbox;
+
+    }
+}
