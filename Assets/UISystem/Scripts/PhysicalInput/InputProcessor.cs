901ed31d74583f26ae4efee4a3cfa9bee4993587
іуко <13497925+gamedevserj@users.noreply.github.com>
2025-11-11 14:36:58 +0400

    Pulled core fix for input being detected when menu is turned off

--- Assets/UISystem/Scripts/PhysicalInput/InputProcessor.cs
+++ Assets/UISystem/Scripts/PhysicalInput/InputProcessor.cs
@@ -11,19 +11,17 @@ namespace UISystem.PhysicalInput
     {
 
         private IInputReceiver _menuInputReceiver;
-        private IInputReceiver _popupInputReceiver;
         private IInputReceiver _activeReceiver;
 
-        private readonly UIInputActions _inputActions;
         private bool CanProcessActions => _activeReceiver != null && _activeReceiver.CanReceivePhysicalInput;
 
         public InputProcessor(UIInputActions inputActions)
         {
-            _inputActions = inputActions;
-            _inputActions.UI.SetCallbacks(this);
+            inputActions.UI.SetCallbacks(this);
             MenusManager.OnControllerSwitch += OnMenuControllerSwitch;
             PopupsManager<PopupResult>.OnControllerSwitch += OnPopupControllerSwitch;
         }
+
         ~InputProcessor()
         {
             MenusManager.OnControllerSwitch -= OnMenuControllerSwitch;
@@ -32,8 +30,7 @@ namespace UISystem.PhysicalInput
 
         private void OnPopupControllerSwitch(IInputReceiver inputReceiver)
         {
-            _popupInputReceiver = inputReceiver;
-            _activeReceiver = _popupInputReceiver ?? _menuInputReceiver;
+            _activeReceiver = inputReceiver ?? _menuInputReceiver;
         }
 
         private void OnMenuControllerSwitch(IInputReceiver inputReceiver)
