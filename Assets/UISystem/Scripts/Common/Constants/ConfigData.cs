38b161c5ab8582a9d5381759597dcb79d37f345e
іуко <13497925+gamedevserj@users.noreply.github.com>
2026-04-28 19:43:09 +0400

    Added ruleset to ignore warnings same as in editorconfig

--- Assets/UISystem/Scripts/Common/Constants/ConfigData.cs
+++ Assets/UISystem/Scripts/Common/Constants/ConfigData.cs
@@ -1,6 +1,9 @@
 ﻿using UISystem.Common.Enums;
 using UnityEngine;
 
+#pragma warning disable SA1201 // Elements should appear in the correct order
+#pragma warning disable SA1516 // Elements should be separated by blank line
+#pragma warning disable SA1600 // Elements should be documented
 namespace UISystem.Constants
 {
     /// <summary>
@@ -8,94 +11,32 @@ namespace UISystem.Constants
     /// </summary>
     public static class ConfigData
     {
-        /// <summary>
-        /// Gets config location.
-        /// </summary>
         public static string ConfigLocation => Application.persistentDataPath + "/" + "Config.txt";
 
-        /// <summary>
-        /// Gets audio section name.
-        /// </summary>
         public const string AudioSectionName = "AudioSettings";
-
-        /// <summary>
-        /// Gets music volume key name.
-        /// </summary>
         public const string MusicVolumeKey = "MusicVolume";
-
-        /// <summary>
-        /// Gets default music volume value.
-        /// </summary>
         public const float DefaultMusicVolume = 0.5f;
-
-        /// <summary>
-        /// Gets sfx volume key name.
-        /// </summary>
         public const string SfxVolumeKey = "SfxVolume";
-
-        /// <summary>
-        /// Gets default music volume value.
-        /// </summary>
         public const float DefaultSfxVolume = 0.5f;
 
-        /// <summary>
-        /// Gets video section name.
-        /// </summary>
         public const string VideoSectionName = "VideoSettings";
-
-        /// <summary>
-        /// Gets resolution key name.
-        /// </summary>
         public const string ResolutionKey = "Resolution";
 
-        /// <summary>
-        /// Gets default resolution value.
-        /// </summary>
         public static Vector2Int DefaultResolution => new(Screen.currentResolution.width, Screen.currentResolution.height);
 
-        /// <summary>
-        /// Gets refresh rate key name.
-        /// </summary>
         public const string RefreshRateKey = "RefreshRate";
-
-        /// <summary>
-        /// Gets default refresh rate value.
-        /// </summary>
         public static int DefaultRefreshRate => Screen.currentResolution.refreshRate;
-
-        /// <summary>
-        /// Gets window mode key name.
-        /// </summary>
         public const string WindowModeKey = "WindowMode";
-
-        /// <summary>
-        /// Gets default full screen value.
-        /// </summary>
         public const FullScreenMode DefaultFullScreenMode = FullScreenMode.ExclusiveFullScreen;
 
-        /// <summary>
-        /// Gets input keys section name.
-        /// </summary>
         public const string KeysSectionName = "Keys";
-
-        /// <summary>
-        /// Gets overrides key name.
-        /// </summary>
         public const string OverridesKey = "Overrides";
 
-        /// <summary>
-        /// Gets interface section name.
-        /// </summary>
         public const string InterfaceSectionName = "Interface";
-
-        /// <summary>
-        /// Gets controller icons key name.
-        /// </summary>
         public const string ControllerIconsKey = "ControllerIcons";
-
-        /// <summary>
-        /// Gets default controller icon type value.
-        /// </summary>
         public const ControllerIconsType DefaultControllerIconsType = ControllerIconsType.Xbox;
     }
 }
+#pragma warning restore SA1201 // Elements should appear in the correct order
+#pragma warning restore SA1516 // Elements should be separated by blank line
+#pragma warning restore SA1600 // Elements should be documented
